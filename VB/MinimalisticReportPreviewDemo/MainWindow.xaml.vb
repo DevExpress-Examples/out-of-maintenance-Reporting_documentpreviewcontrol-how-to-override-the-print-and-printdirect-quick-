bfc56f900e0c55338aa3eca3da99d45cc8def4e9
DevExpressExampleBot <fakeEmail>
2021-10-28 17:05:28 +0300

    autogenerate VB

--- VB/MinimalisticReportPreviewDemo/MainWindow.xaml.vb
+++ VB/MinimalisticReportPreviewDemo/MainWindow.xaml.vb
@@ -1,26 +1,27 @@
-Imports Microsoft.VisualBasic
 Imports System.Windows
 Imports DevExpress.Xpf.Core
 Imports DevExpress.Xpf.Printing
+
 ' ...
-
 Namespace MinimalisticReportPreviewDemo
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
-		Public Sub New()
-			ThemeManager.ApplicationThemeName = "Office2007Silver"
-			InitializeComponent()
-		End Sub
 
-		Private Sub ShowPreview(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			Dim report As New Report()
-			Dim model As New MyXtraReportPreviewModel(report)
-			Dim window As New DocumentPreviewWindow() With {.Model = model}
-			report.CreateDocument(True)
-			window.ShowDialog()
-		End Sub
-	End Class
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits System.Windows.Window
+
+        Public Sub New()
+            ThemeManager.ApplicationThemeName = "Office2007Silver"
+            Me.InitializeComponent()
+        End Sub
+
+        Private Sub ShowPreview(ByVal sender As Object, ByVal e As System.Windows.RoutedEventArgs)
+            Dim report As MinimalisticReportPreviewDemo.Report = New MinimalisticReportPreviewDemo.Report()
+            Dim model As MinimalisticReportPreviewDemo.MyXtraReportPreviewModel = New MinimalisticReportPreviewDemo.MyXtraReportPreviewModel(report)
+            Dim window As DocumentPreviewWindow = New DocumentPreviewWindow() With {.Model = model}
+            report.CreateDocument(True)
+            window.ShowDialog()
+        End Sub
+    End Class
 End Namespace
--- VB/MinimalisticReportPreviewDemo/MyXtraReportPreviewModel.vb
+++ VB/MinimalisticReportPreviewDemo/MyXtraReportPreviewModel.vb
@@ -1,44 +1,46 @@
-Imports Microsoft.VisualBasic
-Imports System
 Imports DevExpress.Xpf.Printing
 Imports DevExpress.XtraReports.UI
 Imports DevExpress.XtraPrinting
 
 Namespace MinimalisticReportPreviewDemo
-	Public Class MyXtraReportPreviewModel
-		Inherits XtraReportPreviewModel
-		Public Sub New(ByVal report As XtraReport)
-			MyBase.New(report)
-		End Sub
-		Protected Overrides Sub Print(ByVal parameter As Object)
-			Dim pt As New PrintTool(Me.Report.PrintingSystemBase)
-			AddHandler pt.PrintingSystem.StartPrint, AddressOf PrintingSystem_StartPrint
-			AddHandler pt.PrintingSystem.PrintProgress, AddressOf PrintingSystem_PrintProgress
-			pt.PrintDialog()
-		End Sub
 
-		Private Sub PrintingSystem_PrintProgress(ByVal sender As Object, ByVal e As PrintProgressEventArgs)
+    Public Class MyXtraReportPreviewModel
+        Inherits XtraReportPreviewModel
 
-		End Sub
-		Private Sub PrintingSystem_StartPrint(ByVal sender As Object, ByVal e As PrintDocumentEventArgs)
+        Public Sub New(ByVal report As XtraReport)
+            MyBase.New(report)
+        End Sub
 
-		End Sub
-		Protected Overrides Sub PrintDirect(ByVal parameter As Object)
-			Dim pt As New PrintTool(Me.Report.PrintingSystemBase)
-			AddHandler pt.PrintingSystem.StartPrint, AddressOf PrintingSystem_StartPrint
-			AddHandler pt.PrintingSystem.PrintProgress, AddressOf PrintingSystem_PrintProgress
-			pt.Print()
-		End Sub
-		Protected Overrides Sub Export(ByVal parameter As Object)
-			If parameter IsNot Nothing Then
-				If parameter.ToString() = "Xls" Then
-					PrintingSystem.ExportToXls("test.xls")
-					System.Windows.MessageBox.Show("!")
-					System.Diagnostics.Process.Start("test.xls")
-				Else
-					MyBase.Export(parameter)
-				End If
-			End If
-		End Sub
-	End Class
+        Protected Overrides Sub Print(ByVal parameter As Object)
+            Dim pt As PrintTool = New PrintTool(Me.Report.PrintingSystemBase)
+            pt.PrintingSystem.StartPrint += New PrintDocumentEventHandler(AddressOf PrintingSystem_StartPrint)
+            pt.PrintingSystem.PrintProgress += New PrintProgressEventHandler(AddressOf PrintingSystem_PrintProgress)
+            pt.PrintDialog()
+        End Sub
+
+        Private Sub PrintingSystem_PrintProgress(ByVal sender As Object, ByVal e As PrintProgressEventArgs)
+        End Sub
+
+        Private Sub PrintingSystem_StartPrint(ByVal sender As Object, ByVal e As PrintDocumentEventArgs)
+        End Sub
+
+        Protected Overrides Sub PrintDirect(ByVal parameter As Object)
+            Dim pt As PrintTool = New PrintTool(Me.Report.PrintingSystemBase)
+            pt.PrintingSystem.StartPrint += New PrintDocumentEventHandler(AddressOf PrintingSystem_StartPrint)
+            pt.PrintingSystem.PrintProgress += New PrintProgressEventHandler(AddressOf PrintingSystem_PrintProgress)
+            pt.Print()
+        End Sub
+
+        Protected Overrides Sub Export(ByVal parameter As Object)
+            If parameter IsNot Nothing Then
+                If Equals(parameter.ToString(), "Xls") Then
+                    PrintingSystem.ExportToXls("test.xls")
+                    System.Windows.MessageBox.Show("!")
+                    System.Diagnostics.Process.Start("test.xls")
+                Else
+                    MyBase.Export(parameter)
+                End If
+            End If
+        End Sub
+    End Class
 End Namespace
